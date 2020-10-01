b32bdf98182925fae2074da4cf45fbf60a1f607f
dvdemon <dvdemon@hotmail.com>
2020-10-01 21:24:28 +0300

    first commit

---
+++ database/chat.h
@@ -0,0 +1,116 @@
+#ifndef CHAT_H
+#define CHAT_H
+
+#include "Poco/JSON/Object.h"
+#include <string>
+#include <vector>
+#include "database_mysql.h"
+#include <time.h>
+
+namespace database{
+    struct Chat{
+            long        id;
+            std::time_t time;
+            std::string source_login;
+            std::string destination_login;
+            std::string message;
+            long        readed;
+
+
+            Poco::JSON::Object::Ptr toJSON() const{
+                Poco::JSON::Object::Ptr root = new Poco::JSON::Object();
+                root->set("id",id);
+                root->set("time",time);
+                root->set("source_login",source_login);
+                root->set("destination_login",destination_login);
+                root->set("message",message);
+                root->set("readed",readed);
+                return root;
+            }
+
+            static void init(){
+                try{
+                    std::cout << "initializing chat ..." << std::endl;
+                    std::string query = "CREATE TABLE `chat` (`id` INT NOT NULL AUTO_INCREMENT,`time` DATETIME NOT NULL, `source_login` VARCHAR(256) NOT NULL,`destination_login` VARCHAR(256) NOT NULL,`message` VARCHAR(1024) NULL,  `readed` int NOT NULL,PRIMARY KEY (`id`),KEY `source` (`source_login`),KEY `destination` (`destination_login`));";
+                    database::Database_MySQL::get().execute(query);
+                    std::cout << "initializing char ... done" << std::endl;
+                }catch(...){
+                    std::cout << "initializing chat ... fail" << std::endl;
+                }
+
+            }
+
+
+            static std::vector<Chat> get(const std::string &login){
+                std::vector<Chat> result;
+                Chat chat;
+
+                std::string query="SELECT id, UNIX_TIMESTAMP(time) as tt, source_login,destination_login,message,readed FROM chat WHERE source_login='";
+                query += login;
+                query += "' OR destination_login='";
+                query += login;
+                query += "';";
+                database::Database_MySQL::get().query(query,[&](int ,int column,std::string value)
+                {
+                        switch(column){
+                            case 0: chat.id = atoi(value.c_str()); break;
+                            case 1: chat.time= atol(value.c_str());break;
+                            case 2: chat.source_login=value;break;
+                            case 3: chat.destination_login=value;break;
+                            case 4: chat.message=value;break;
+                            case 5: chat.readed=atoi(value.c_str());break;
+                        }
+                    
+                },[&](int){
+                    result.push_back(chat);
+                });
+                
+
+                return result;
+            }
+
+            static std::vector<Chat> get(const std::string &login1,const std::string &login2){
+                std::vector<Chat> result;
+                Chat chat;
+
+                std::string query="SELECT id, UNIX_TIMESTAMP(time) as tt, source_login,destination_login,message,readed FROM chat WHERE (source_login='";
+                query += login1;
+                query += "' AND destination_login='";
+                query += login2;
+                query += "') OR (source_login='";
+                query += login2;
+                query += "' AND destination_login='";
+                query += login1;
+                query += "');";
+                database::Database_MySQL::get().query(query,[&](int ,int column,std::string value)
+                {
+                        switch(column){
+                            case 0: chat.id = atoi(value.c_str()); break;
+                            case 1: chat.time= atol(value.c_str());break;
+                            case 2: chat.source_login=value;break;
+                            case 3: chat.destination_login=value;break;
+                            case 4: chat.message=value;break;
+                            case 5: chat.readed=atoi(value.c_str());break;
+                        }
+                    
+                },[&](int){
+                    result.push_back(chat);
+                });
+                
+
+                return result;
+            }
+
+            void insert(){
+                std::string query="INSERT INTO chat  (time,source_login,destination_login,message,readed) VALUES(";
+                query+="FROM_UNIXTIME("+std::to_string(time)+"),";
+                query+="'"+source_login+"',";
+                query+="'"+destination_login+"',";
+                query+="'"+message+"',";
+                query+="'"+std::to_string(readed)+"'";
+                query+=")";
+                database::Database_MySQL::get().execute(query);
+            }
+    };
+}
+#endif
---
+++ web_server/handlers/ChatHandler.h
@@ -0,0 +1,148 @@
+        #ifndef CHATHANDLER_H
+        #define CHATHANDLER_H
+
+        #include "Poco/Net/HTTPServer.h"
+        #include "Poco/Net/HTTPRequestHandler.h"
+        #include "Poco/Net/HTTPRequestHandlerFactory.h"
+        #include "Poco/Net/HTTPServerParams.h"
+        #include "Poco/Net/HTTPServerRequest.h"
+        #include "Poco/Net/HTTPServerResponse.h"
+        #include "Poco/Net/HTTPServerParams.h"
+        #include "Poco/Net/HTMLForm.h"
+        #include "Poco/Net/ServerSocket.h"
+        #include "Poco/Timestamp.h"
+        #include "Poco/DateTimeFormatter.h"
+        #include "Poco/DateTimeFormat.h"
+        #include "Poco/Exception.h"
+        #include "Poco/ThreadPool.h"
+        #include "Poco/Util/ServerApplication.h"
+        #include "Poco/Util/Option.h"
+        #include "Poco/Util/OptionSet.h"
+        #include "Poco/Util/HelpFormatter.h"
+        #include <iostream>
+        #include <iostream>
+        #include <fstream>
+
+        using Poco::Net::ServerSocket;
+        using Poco::Net::HTTPRequestHandler;
+        using Poco::Net::HTTPRequestHandlerFactory;
+        using Poco::Net::HTTPServer;
+        using Poco::Net::HTTPServerRequest;
+        using Poco::Net::HTTPServerResponse;
+        using Poco::Net::HTTPServerParams;
+        using Poco::Timestamp;
+        using Poco::DateTimeFormatter;
+        using Poco::DateTimeFormat;
+        using Poco::ThreadPool;
+        using Poco::Util::ServerApplication;
+        using Poco::Util::Application;
+        using Poco::Util::Option;
+        using Poco::Util::OptionSet;
+        using Poco::Util::OptionCallback;
+        using Poco::Util::HelpFormatter;
+        using Poco::Net::NameValueCollection;
+        using Poco::Net::HTMLForm;
+
+        #include "../../database/chat.h"
+        #include "../session/session.h"
+
+        #include <map>
+
+        class ChatHandler: public HTTPRequestHandler
+        {
+        public:
+            ChatHandler(const std::string& format): _format(format)
+            {
+            }
+
+            void handleRequest(HTTPServerRequest& request,
+                            HTTPServerResponse& response)
+            {
+                HTMLForm form(request);
+                response.setChunkedTransferEncoding(true);
+                response.setContentType("application/json");
+                std::ostream& ostr = response.send();
+                if(form.has("session_id") && !form.has("add") && !form.has("filter")){
+                    std::string session_str=form.get("session_id"); 
+                    long  session_id = stol(session_str);
+
+                    if(webserver::Session::get().is_correct(session_id)){
+                        try{
+                            std::string login = webserver::Session::get().get_login(session_id);
+                            std::vector<database::Chat> friends = database::Chat::get(login); 
+                            Poco::JSON::Array arr;
+                            for(auto s: friends) {
+                                Poco::JSON::Object::Ptr ptr = new Poco::JSON::Object();
+                                ptr->set("id",s.id);
+                                ptr->set("time",s.time);
+                                ptr->set("source_login",s.source_login);
+                                ptr->set("destination_login",s.destination_login);
+                                ptr->set("message",s.message);
+                                ptr->set("readed",s.readed);
+                                arr.add(ptr);   
+                            }      
+                            Poco::JSON::Stringifier::stringify(arr,ostr);
+                        }catch(...){
+                        }
+                    } 
+                } else
+                if(form.has("session_id") && (!form.has("add")) && form.has("filter")){
+                    std::string session_str=form.get("session_id"); 
+                    long  session_id = stol(session_str);
+
+                    if(webserver::Session::get().is_correct(session_id)){
+                        try{
+                            std::string login = webserver::Session::get().get_login(session_id);
+                            std::string filter = form.get("filter");
+                            std::vector<database::Chat> friends = database::Chat::get(login,filter); 
+                            Poco::JSON::Array arr;
+                            for(auto s: friends) {
+                                Poco::JSON::Object::Ptr ptr = new Poco::JSON::Object();
+                                ptr->set("id",s.id);
+                                ptr->set("time",s.time);
+                                ptr->set("source_login",s.source_login);
+                                ptr->set("destination_login",s.destination_login);
+                                ptr->set("message",s.message);
+                                ptr->set("readed",s.readed);
+                                arr.add(ptr);   
+                            }      
+                            Poco::JSON::Stringifier::stringify(arr,ostr);
+                        }catch(...){
+                        }
+                    } 
+                } else
+                if(form.has("session_id")  
+                   && form.has("add")
+                   && form.has("destination")
+                   && form.has("message")){
+                    std::cout << "add chat" << std::endl;
+                    std::string session_str=form.get("session_id"); 
+                    long  session_id = stol(session_str);
+
+                    if(webserver::Session::get().is_correct(session_id)){
+                        try{
+                            std::string login = webserver::Session::get().get_login(session_id);
+                            database::Chat chat;
+                            chat.source_login = login;
+                            chat.destination_login = form.get("destination");
+                            time (&chat.time);
+                            chat.message = form.get("message");
+                            chat.readed = 0;
+
+                            chat.insert();
+
+                            Poco::JSON::Object::Ptr ptr = new Poco::JSON::Object();
+                            ptr->set("inserted","true");
+                            Poco::JSON::Stringifier::stringify(ptr,ostr);
+                        }catch(std::exception &ex){
+                            std::cout << "exception:" << ex.what() << std::endl;
+                        }
+                    } 
+                } else
+                response.setStatus(Poco::Net::HTTPResponse::HTTPStatus::HTTP_INTERNAL_SERVER_ERROR);
+            }
+
+        private:
+            std::string _format;
+        };
+        #endif // !CHATHANDLER_H
